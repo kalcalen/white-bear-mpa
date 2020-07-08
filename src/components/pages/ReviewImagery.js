958d8cbecb1a7e0bc29a75ca24b5333bd8baa522
kalcalen <kalcalen@gmail.com>
2020-07-08 10:45:47 -0700

    added ReviewImagery

--- src/components/pages/ReviewImagery.js
+++ src/components/pages/ReviewImagery.js
@@ -8,6 +8,29 @@ export default function ReviewImagery() {
       <AppTemplate>
          <Header />
          <Navigation />
+         <div class="mb-2">
+            <div class="card bg-primary mb-5">
+               <div class="card-body">
+                  One morning, when Gregor Samsa woke from troubled dreams, he
+                  found himself transformed in his bed into a horrible vermin.
+                  He lay on his armour-like back, and if he lifted his head a
+                  little he could see his brown belly, slightly domed and
+                  divided by arches into stiff sections. The bedding was hardly.
+               </div>
+            </div>
+         </div>
+
+         <div class="clearfix"></div>
+
+         <button class="btn btn-link">Previous card</button>
+         <div class="float-right">
+            <a
+               href="review-answer.html"
+               class="btn btn-outline-primary float-right"
+            >
+               Show answer
+            </a>
+         </div>
       </AppTemplate>
    );
 }
