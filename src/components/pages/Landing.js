afc19a839abb22bc958c63cb073850b3cfea6210
kalcalen <kalcalen@gmail.com>
2020-07-08 14:24:20 -0700

    added className to landing

--- src/components/pages/Landing.js
+++ src/components/pages/Landing.js
@@ -5,51 +5,54 @@ export default function Landing() {
    return (
       <>
          <div className="background-image">
-            <div class="container">
-               <div class="row">
+            <div className="container">
+               <div className="row">
                   <img src={landingBear} alt="" />
-                  <h1 class="index-logo">White Bear</h1>
+                  <h1 className="index-logo">White Bear</h1>
                </div>
 
-               <div class="row card-index">
-                  <div class="col-xl-5 offset-xl-1 col-sm-8 col-lg-8 offset-4 offset-sm-2 offset-md-2 col-10 offset-1">
-                     <div class="card mb-5">
-                        <div class="card-body landing-text">
-                           <h2 class="card-title">Nice to meet you</h2>
-                           <p class="mb-2">Sign up for White Bear.</p>
-                           <p class="mb-3">Free forever.</p>
+               <div className="row card-index">
+                  <div className="col-xl-5 offset-xl-1 col-sm-8 col-lg-8 offset-4 offset-sm-2 offset-md-2 col-10 offset-1">
+                     <div className="card mb-5">
+                        <div className="card-body landing-text">
+                           <h2 className="card-title">Nice to meet you</h2>
+                           <p className="mb-2">Sign up for White Bear.</p>
+                           <p className="mb-3">Free forever.</p>
                            <button
                               id="sign-up"
-                              class="btn btn-block btn-success mb-4"
+                              className="btn btn-block btn-success mb-4"
                            >
                               Let's get you signed up
                            </button>
 
                            <div id="form-toggle">
-                              <label class="text-muted" for="email">
+                              <label className="text-muted" for="email">
                                  Email Address
                               </label>
                               <input
-                                 class="form-control mb-3"
+                                 className="form-control mb-3"
                                  type="email"
                                  id="emailCreate"
                                  name="email"
                               />
 
-                              <p class="text-danger" id="emailError"></p>
-                              <label class="text-muted" for="password">
+                              <p className="text-danger" id="emailError"></p>
+                              <label className="text-muted" for="password">
                                  Password
                               </label>
                               <input
-                                 class="form-control mb-3 mt-3"
+                                 className="form-control mb-3 mt-3"
                                  type="password"
                                  id="passwordCreate"
                                  name="password"
                               />
-                              <div class="text-danger" id="passwordError"></div>
+                              <div
+                                 className="text-danger"
+                                 id="passwordError"
+                              ></div>
                               <a
                                  id="letsGo"
-                                 class="btn btn-success btn-block float-right mt-3"
+                                 className="btn btn-success btn-block float-right mt-3"
                                  href="create-answer"
                               >
                                  Lets go!
@@ -59,35 +62,35 @@ export default function Landing() {
                      </div>
                   </div>
 
-                  <div class="col-xl-5 offset-xl-1 col-sm-8 col-lg-8 offset-sm-2 col-10 offset-1">
-                     <div class="card mb-5">
-                        <div class="card-body landing-text">
-                           <h2 class="card-title">Welcome Back</h2>
-                           <p class="mb-3">
+                  <div className="col-xl-5 offset-xl-1 col-sm-8 col-lg-8 offset-sm-2 col-10 offset-1">
+                     <div className="card mb-5">
+                        <div className="card-body landing-text">
+                           <h2 className="card-title">Welcome Back</h2>
+                           <p className="mb-3">
                               Log-in with your email address and password.
                            </p>
-                           <form class="form-group">
-                              <label class="text-muted" for="email">
+                           <form className="form-group">
+                              <label className="text-muted" for="email">
                                  Email Address
                               </label>
                               <input
-                                 class="form-control"
+                                 className="form-control"
                                  type="email"
                                  id="emailWelcome"
                                  name="email"
                               />
-                              <label class="text-muted" for="password">
+                              <label className="text-muted" for="password">
                                  Password
                               </label>
                               <input
-                                 class="form-control mb-3"
+                                 className="form-control mb-3"
                                  type="password"
                                  id="passwordWelcome"
                                  name="password"
                               />
                               <a
                                  href="create-answer"
-                                 class="btn btn-success float-right"
+                                 className="btn btn-success float-right"
                               >
                                  Log in
                               </a>
