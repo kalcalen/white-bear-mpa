87423090a2b6d8b7b7530a005b289a7a31fc5456
kalcalen <kalcalen@gmail.com>
2020-07-07 21:16:39 -0700

    Linked landing page

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,14 +1,15 @@
 import React from "react";
 import appLogo from "../../icons/logo-app.svg";
+import { Link } from "react-router-dom";
 
 export default function Header() {
    return (
       <div>
          <img src={appLogo} width="32px" alt="White Bear logo" />
          <h3 className="d-inline text-brand ml-1">White Bear</h3>
-         <button href="index.html" className="btn btn-link float-right">
+         <Link to="/" className="btn btn-link float-right">
             Log out
-         </button>
+         </Link>
       </div>
    );
 }
